33b705defaa1adbe594bcdcd7bed6d4a90fc26eb
@specktro <specktro@nonull.mx>
2025-08-21 12:00:59 -0600

    feat: Add simple binary search solution algorithm

--- algo.playground/Contents.swift
+++ algo.playground/Contents.swift
@@ -1,1 +1,27 @@
+import Foundation
 
+/// A simple binary search implementation
+func binarySearch(arr: [Int] ,item: Int) -> Int {
+    var low = 0
+    var high = arr.count - 1
+    
+    while low <= high {
+        let mid = (low + high) / 2
+        let guess = arr[mid]
+        
+        if guess == item {
+            print("Found at index \(mid)")
+            return mid
+        } else if guess < item {
+            low = mid + 1
+        } else {
+            high = mid - 1
+        }
+    }
+    
+    return -1
+}
+
+let myList = [1, 3, 5, 7, 9]
+print(binarySearch(arr: myList, item: 5))
+print(binarySearch(arr: myList, item: -1))
