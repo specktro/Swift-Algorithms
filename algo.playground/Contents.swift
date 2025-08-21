eaa46504431773486569bc6f4c3567515ffea22b
@specktro <specktro@nonull.mx>
2025-08-21 12:01:55 -0600

    feat: Add a simple recursive sum function sample

--- algo.playground/Contents.swift
+++ algo.playground/Contents.swift
@@ -25,3 +25,16 @@ func binarySearch(arr: [Int] ,item: Int) -> Int {
 let myList = [1, 3, 5, 7, 9]
 print(binarySearch(arr: myList, item: 5))
 print(binarySearch(arr: myList, item: -1))
+
+/// sum recursive function
+func recursiveSum(_ nums: [Int]) -> Int {
+    if nums.isEmpty {
+        return 0
+    } else {
+        return nums.first! + recursiveSum(Array(nums.dropFirst()))
+    }
+}
+
+let myNums = [2, 4, 6]
+print("Recursive sum: \(recursiveSum(myNums))")
+
